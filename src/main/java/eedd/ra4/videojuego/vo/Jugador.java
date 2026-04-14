48ff45d25ce95342f6296850c762c9df3112b81c
Sophie Fajardo Rodriguez <sophie@luminessa.net>
2026-04-14 16:06:52 +0200

    refactor: Extraidas constantes de clase Jugador

--- src/main/java/eedd/ra4/videojuego/vo/Jugador.java
+++ src/main/java/eedd/ra4/videojuego/vo/Jugador.java
@@ -10,18 +10,22 @@ package eedd.ra4.videojuego.vo;
  * 
  */
 public class Jugador extends Personaje {
-    public int experiencia;
+
+	private static final int MULTIPLICADOR_DANIO = 20;
+	private static final int EXPERIENCIA_SUBIR_NIVEL = 100;
+	
+	public int experiencia;
 
     public void ganarExperiencia(int puntos) {
         experiencia += puntos;
-        if (experiencia > 100) {
+        if (experiencia > Jugador.EXPERIENCIA_SUBIR_NIVEL) {
             nivel++;
             experiencia = 0;
         }
     }
     
     public void ataqueEspecial(Enemigo enemigo) {
-        int danio = nivel * 20;
+        int danio = nivel * Jugador.MULTIPLICADOR_DANIO;
         enemigo.vida -= danio;
     }
 }
